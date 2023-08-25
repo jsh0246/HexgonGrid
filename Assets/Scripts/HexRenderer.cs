cebcbd5fb5ef1ece43636682dcf49b6417e9c1a6
jsh0246 <jsh0246@naver.com>
2023-08-25 09:59:35 +0900

    dd

--- Assets/Scripts/HexRenderer.cs
+++ Assets/Scripts/HexRenderer.cs
@@ -24,10 +24,13 @@ public class HexRenderer : MonoBehaviour
     private Mesh m_mesh;
     private MeshFilter m_meshFilter;
     private MeshRenderer m_meshRenderer;
-
     private List<Face> m_faces;
 
     public Material material;
+    public float innerSize;
+    public float outerSize;
+    public float height;
+    public bool isFlatTopped;
 
     private void Awake()
     {
@@ -46,13 +49,13 @@ public class HexRenderer : MonoBehaviour
         DrawMesh();
     }
 
-    //public void OnValidate()
-    //{
-    //    if(Application.isPlaying)
-    //    {
-    //        DrawMesh();
-    //    }
-    //}
+    public void OnValidate()
+    {
+        if (Application.isPlaying)
+        {
+            DrawMesh();
+        }
+    }
 
     public void DrawMesh()
     {
@@ -67,7 +70,25 @@ public class HexRenderer : MonoBehaviour
         // Top faces
         for(int point =0; point<6; point++)
         {
+            m_faces.Add(CreateFace(innerSize, outerSize, height / 2f, height / 2f, point));
+        }
 
+        // Bottom faces
+        for (int point = 0; point < 6; point++)
+        {
+            m_faces.Add(CreateFace(innerSize, outerSize, -height / 2f, -height / 2f, point, true));
+        }
+
+        // Outer faces
+        for (int point = 0; point < 6; point++)
+        {
+            m_faces.Add(CreateFace(outerSize, outerSize, height / 2f, -height / 2f, point, true));
+        }
+
+        // Inner faces
+        for (int point = 0; point < 6; point++)
+        {
+            m_faces.Add(CreateFace(innerSize, innerSize, -height / 2f, -height / 2f, point));
         }
     }
 
@@ -91,21 +112,43 @@ public class HexRenderer : MonoBehaviour
             }
         }
 
-        m_mesh.vertices = vertices.ToArray();
-        m_mesh.triangles = tris.ToArray();
-        m_mesh.uv = uvs.ToArray();
-        m_mesh.RecalculateNormals();
+        if (m_mesh)
+        {
+            m_mesh.vertices = vertices.ToArray();
+            m_mesh.triangles = tris.ToArray();
+            m_mesh.uv = uvs.ToArray();
+            m_mesh.RecalculateNormals();
+        }
     }
 
     private Face CreateFace(float innerRad, float outerRad, float heightA, float heightB, int point, bool reverse = false)
     {
-        return new Face();
+        Vector3 pointA = GetPoint(innerRad, heightB, point);
+        Vector3 pointB = GetPoint(innerRad, heightB, (point < 5) ? point + 1 : 0);
+        Vector3 pointC = GetPoint(outerRad, heightA, (point < 5) ? point + 1 : 0);
+        Vector3 pointD = GetPoint(outerRad, heightA, point);
+
+        List<Vector3> vertices = new List<Vector3>() { pointA, pointB, pointC, pointD };
+        List<int> triangles = new List<int>() { 0, 1, 2, 2, 3, 0 };
+        List<Vector2> uvs = new List<Vector2>() { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) };
+
+        if (reverse)
+        {
+            vertices.Reverse();
+        }
+
+        return new Face(vertices, triangles, uvs);
     }
 
     protected Vector3 GetPoint(float size, float height, int index)
     {
-        float angle_deg = 60 * index;
+        float angle_deg = isFlatTopped ? 60 * index : 60 * index - 30;
         float angle_rad = Mathf.PI / 180f * angle_deg;
         return new Vector3((size * Mathf.Cos(angle_rad)), height, size * Mathf.Sin(angle_rad));
+    }
+
+    public void SetMaterial(Material newMaterial)
+    {
+        m_meshRenderer.material = newMaterial;
     }
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -9,51 +9,51 @@ public class Test : MonoBehaviour
     {
         Mesh mesh = new Mesh();
 
-        //Vector3[] vertices = new Vector3[3];
-        //Vector2[] uvs = new Vector2[3];
-        //int[] triangles = new int[3];
-
-        //vertices[0] = new Vector3(0, 0, 0);
-        //vertices[1] = new Vector3(1, 0, 0);
-        //vertices[2] = new Vector3(0, 0, 1);
-
-        ////vertices[0] = new Vector3(0, 0, 0);
-        ////vertices[1] = new Vector3(100, 0, 0);
-        ////vertices[2] = new Vector3(0, 0, 100);
-
-        //triangles[0] = 0;
-        //triangles[1] = 1;
-        //triangles[2] = 2;
-
-        //uvs[0] = new Vector2(0, 0);
-        //uvs[1] = new Vector2(1, 0);
-        //uvs[2] = new Vector2(0, 1);
-
-
-
-
-
-
-        Vector3[] vertices = new Vector3[4];
-        Vector2[] uvs = new Vector2[4];
-        int[] triangles = new int[6];
+        Vector3[] vertices = new Vector3[3];
+        Vector2[] uvs = new Vector2[3];
+        int[] triangles = new int[3];
 
         vertices[0] = new Vector3(0, 0, 0);
         vertices[1] = new Vector3(1, 0, 0);
-        vertices[2] = new Vector3(1, 0, 1);
-        vertices[3] = new Vector3(0, 0, 1);
+        vertices[2] = new Vector3(0, 0, 1);
+
+        //vertices[0] = new Vector3(0, 0, 0);
+        //vertices[1] = new Vector3(100, 0, 0);
+        //vertices[2] = new Vector3(0, 0, 100);
 
         triangles[0] = 0;
         triangles[1] = 2;
         triangles[2] = 1;
-        triangles[3] = 0;
-        triangles[4] = 3;
-        triangles[5] = 2;
 
         uvs[0] = new Vector2(0, 0);
         uvs[1] = new Vector2(1, 0);
         uvs[2] = new Vector2(1, 1);
-        uvs[3] = new Vector2(0, 1);
+
+
+
+
+
+
+        //Vector3[] vertices = new Vector3[4];
+        //Vector2[] uvs = new Vector2[4];
+        //int[] triangles = new int[6];
+
+        //vertices[0] = new Vector3(0, 0, 0);
+        //vertices[1] = new Vector3(1, 0, 0);
+        //vertices[2] = new Vector3(1, 0, 1);
+        //vertices[3] = new Vector3(0, 0, 1);
+
+        //triangles[0] = 0;
+        //triangles[1] = 2;
+        //triangles[2] = 1;
+        //triangles[3] = 0;
+        //triangles[4] = 3;
+        //triangles[5] = 2;
+
+        //uvs[0] = new Vector2(0, 0);
+        //uvs[1] = new Vector2(1, 0);
+        //uvs[2] = new Vector2(1, 1);
+        //uvs[3] = new Vector2(0, 1);
 
         mesh.vertices = vertices;
         mesh.uv = uvs;
